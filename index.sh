4ff851efe85acd9f1a0d05befca09078ee068588
Jeyson Mueses <andruudev@gmail.com>
2025-01-15 23:27:36 -0500

    Writing prework

---
+++ index.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Configuración
+ORIGEN="/ruta/origen/"
+DESTINO="/ruta/destino/"
+REPORTE="/ruta/reporte.txt"
+EMAIL="usuario@example.com"
+
+
+# 1. Funcion: Limpiar Temporales - Juliàn Solòrzano
+# 2. Funcion: Actualizar Sistema - Valentin Vargas
+# 3. Funcion: Sincronizar Archivos
+# 4. Funcion: Programar Tareas - Josue Cruz
+# 5. Funcion: Enviar Reporte - Jeyson Mueses
